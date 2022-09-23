f90dfccf7ec70bb4d8a326df17efc00a04c5210f
Enrique Marin <enrike2190@gmail.com>
2022-09-23 11:30:18 +0200

    UwU

---
+++ HANDS ON 1.R
@@ -0,0 +1,12 @@
+
+# ID SCRIP ----------------------------------------------------------------
+
+## LENGUAJES DE PROGRAMACIÓN ESTADÍSTICA
+## PROFESOR : CHRISTIAN SUCUZHANNAY ARÉVALO
+## ALUMNO : ENRIQUE MARÍN SÁNCHEZ, EXP 22076186
+## HANDS ON 01
+
+# LOADING LIBS ------------------------------------------------------------
+
+install.packages("tidyverse","httr","janitor")
+
